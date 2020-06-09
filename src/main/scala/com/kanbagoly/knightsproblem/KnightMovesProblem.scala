5f711d2e376a09d2a767c5a921f179161458a988
Kanbagoly <kanbagoly@gmail.com>
2020-06-09 03:04:56 +0200

    Simplify algo

--- src/main/scala/com/kanbagoly/knightsproblem/KnightMovesProblem.scala
+++ src/main/scala/com/kanbagoly/knightsproblem/KnightMovesProblem.scala
@@ -2,12 +2,47 @@ package com.kanbagoly.knightsproblem
 
 import com.kanbagoly.knightsproblem.KeyPad.{buttonIndices, isKey, isVowel, keyId}
 
+import scala.collection.immutable.ArraySeq
 import scala.collection.mutable
+
+private object KeyPad {
+
+  private sealed trait Key
+  private case object Vowel extends Key
+  private case object Consonant extends Key
+  private case object Empty extends Key
+
+  private val Buttons: ArraySeq[ArraySeq[Key]] = ArraySeq(
+    ArraySeq(Vowel, Consonant, Consonant, Consonant, Vowel),
+    ArraySeq(Consonant, Consonant, Consonant, Vowel, Consonant),
+    ArraySeq(Consonant, Consonant, Consonant, Consonant, Vowel),
+    ArraySeq(Empty, Consonant, Consonant, Consonant, Empty)
+  )
+
+  private val Rows = Buttons.size
+  private val Columns = Buttons.head.size
+
+  def buttonIndices: Seq[(Int, Int)] =
+    for {
+      x <- Buttons.indices
+      y <- Buttons(x).indices
+      if Buttons(x)(y) != Empty
+    } yield (x ,y)
+
+  def isKey(row: Int, column: Int): Boolean =
+    0 <= row && row < Rows &&
+      0 <= column && column < Columns &&
+      Buttons(row)(column) != Empty
+
+  def isVowel(row: Int, column: Int): Boolean = Buttons(row)(column) == Vowel
+
+  def keyId(row: Int, column: Int): Int = row * Columns + column
+
+}
 
 object KnightMovesProblem {
 
-  def solve(maxLength: Int): Long =
-    solve(maxLength, mutable.HashMap.empty[(Int, Int, Int), Long])
+  def solve(maxLength: Int): Long = solve(maxLength, mutable.HashMap.empty)
 
   private def solve(maxLength: Int, memo: mutable.HashMap[(Int, Int, Int), Long]): Long = {
     def compute(row: Int, column: Int, length: Int, vowels: Int): Long =
